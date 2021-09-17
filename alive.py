c1e76be94190b89db140d82a9a96a5e79dd7445d
saurab6 <saurabmia6@gmail.com>
2021-09-17 18:18:11 +0530

    minor changes

--- alive.py
+++ alive.py
@@ -5,24 +5,15 @@
 import time
 import requests
 import os
-import logging
 
-logging.info("GETTING BASE URL")
-try:
-    BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
-    logging.info(BASE_URL)
-except Exception:
-    logging.info("ERROR WHILE GETTING BASE_URL")
+BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
 try:
     if len(BASE_URL) == 0:
         BASE_URL = None
 except:
     BASE_URL = None
 PORT = os.environ.get('PORT', None)
-print(str(PORT))
-print("checking")
 if PORT is not None and BASE_URL is not None:
     while True:
-        print("OHH YEAH")
-        time.sleep(10)
+        time.sleep(600)
         status = requests.get(BASE_URL).status_code
